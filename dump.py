29985e3fb7cc4ac3e5f6a075cf6b51c197ac7ede
Brian Osgood <osgood24b@gmail.com>
2018-07-10 22:52:22 -0700

    Updated dump.py and requrements.txt
    There was a change in the dataset package, freeze was moved to datafreeze

--- dump.py
+++ dump.py
@@ -1,9 +1,10 @@
 import settings
 import tweepy
 import dataset
+from datafreeze import freeze
 from textblob import TextBlob
 
 db = dataset.connect(settings.CONNECTION_STRING)
 
 result = db[settings.TABLE_NAME].all()
-dataset.freeze(result, format='csv', filename=settings.CSV_NAME)
+freeze(result, format='csv', filename=settings.CSV_NAME)
